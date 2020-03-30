6b98b4feef4978c16019319a03772db0e054d315
helloztq <ztq88@hotmail.com>
2020-03-31 01:34:11 +0800

    modify

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -84,12 +84,22 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# print(dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     ))
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
 
 
 # Password validation
