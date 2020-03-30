15ee5d98620fe83ec0384753758e73545e2f1978
helloztq <ztq88@hotmail.com>
2020-03-31 01:41:27 +0800

    modify

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -142,6 +142,8 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
