f52932ce10376923439f527bc14fa550c6f35272
helloztq <ztq88@hotmail.com>
2020-03-31 10:36:38 +0800

    modify

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -144,7 +144,7 @@ LOGIN_URL = 'login'
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
